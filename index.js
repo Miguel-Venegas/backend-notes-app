524ca66ca8d210bac1f47c481d57c2b1728cbaac
Miguel-Venegas <venegas.a.miguel@gmail.com>
2025-11-04 15:25:08 +0700

    finish

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 
 let notes = [
@@ -19,11 +22,75 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-})
+// get method
+
+app.get('/', (request, response) => {
+    response.send('<h1>hello world</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+});
+
+//  a single note from resources
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    console.log('id: ', id);
+    const note = notes.find(note => note.id === id);
+
+    // send note or 404 code
+    if (note) {
+        response.send(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+// delete
+
+app.delete('/api/notes/:id', (request, response) => {
+
+    const id = request.params.id;
+    notes = notes.filter(note => note.id !== id);
+    response.status(204).end();
+});
+
+// post
+
+const generateId = () => {
+    const maxId = Math.max(...notes.map(note => Number(note.id)));
+
+    return String(maxId + 1);
+
+};
+
+app.post('/api/notes', (request, response) => {
+
+   
+
+    const body = request.body;
+    
+
+    if (!body.content) {
+        return response.status(400).json(
+            {error: 'content missing'}
+        );
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+    
+    notes = notes.concat(note);
+
+    response.json(note);
+});
+
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log("server running on port ", PORT)
+});
