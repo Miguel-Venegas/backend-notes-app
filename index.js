d508419664a058cf2c89fb5f43186d056aa44d9c
Miguel-Venegas <venegas.a.miguel@gmail.com>
2025-11-07 13:30:07 +0700

    finish connecting database with express node

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+require('dotenv').config();
 const express = require("express");
-
 const app = express();
+const Note = require('./models/note');
 
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    },
-    {
-        id: "4",
-        content: "Express is easy",
-        important: false
-    }
-]
+
+
+
+
+// let notes = [
+//     {
+//         id: "1",
+//         content: "HTML is easy",
+//         important: true
+//     },
+//     {
+//         id: "2",
+//         content: "Browser can execute only JavaScript",
+//         important: false
+//     },
+//     {
+//         id: "3",
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         important: true
+//     },
+//     {
+//         id: "4",
+//         content: "Express is easy",
+//         important: false
+//     }
+// ]
 
 // get method
 
@@ -35,68 +40,61 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    });
+    // response.json(notes);
 });
 
 //  a single note from resources
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    console.log('id: ', id);
-    const note = notes.find(note => note.id === id);
-
-    // send note or 404 code
-    if (note) {
-        response.send(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 });
 
 // delete
 
 app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
 
-    const id = request.params.id;
-    notes = notes.filter(note => note.id !== id);
-    response.status(204).end();
-});
+    Note.findByIdAndDelete(id)
+        .then(result => {
+            if (result) {
+                response.status(200).send(`Deleted note: ${result.content}`)
+            } else {
+                response.status(404).json({ error: 'Note not found' })
+            }
+        })
+        .catch(error => {
+            console.error(error)
+            response.status(400).json({ error: 'malformatted id' })
+        })
+})
+
 
 // post
 
-const generateId = () => {
-    const maxId = Math.max(...notes.map(note => Number(note.id)));
-
-    return String(maxId + 1);
-
-};
-
 app.post('/api/notes', (request, response) => {
-
-   
-
-    const body = request.body;
-    
+    const body = request.body
 
     if (!body.content) {
-        return response.status(400).json(
-            {error: 'content missing'}
-        );
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
-    
-    notes = notes.concat(note);
+    })
 
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 });
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
